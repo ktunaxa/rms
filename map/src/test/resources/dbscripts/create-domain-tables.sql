488b1020b457a7a74f986e66947cb7c20464d777
pieterdg <pieterdg@cebd4ea5-dd72-4036-80b6-269358829854>
2011-02-07 15:58:18 +0000

    KTU-12: Define Data Model for the RMS system	Add Template classes
    git-svn-id: https://svn.geomajas.org/geosparc/projects/ktunaxa@758 cebd4ea5-dd72-4036-80b6-269358829854

--- map/src/test/resources/dbscripts/create-domain-tables.sql
+++ map/src/test/resources/dbscripts/create-domain-tables.sql
@@ -5,13 +5,14 @@
 DROP TABLE template;
 DROP TABLE document_comment;
 DROP TABLE referral_comment;
--- DROP TABLE comment;
 DROP TABLE document;
+DROP TABLE document_type;
 DROP TABLE reference_key_value;
 DROP TABLE reference_aspect;
 DROP TABLE reference;
-DROP TABLE deadline;
 DROP TABLE referral;
+DROP TABLE referral_application_type;
+DROP TABLE referral_type;
 DROP TABLE referral_status;
 DROP TABLE aspect;
 
@@ -30,9 +31,11 @@ ALTER TABLE aspect OWNER TO ktunaxa;
 GRANT ALL ON TABLE aspect TO ktunaxa;
 GRANT ALL ON TABLE aspect TO postgres;
 
-INSERT INTO aspect (id, description) values (1, 'Culture');
-INSERT INTO aspect (id, description) values (2, 'Ecology');
-INSERT INTO aspect (id, description) values (3, 'Archaeology');
+INSERT INTO aspect (id, description) values (1, 'Aquatic');
+INSERT INTO aspect (id, description) values (2, 'Archaeological');
+INSERT INTO aspect (id, description) values (3, 'Cultural');
+INSERT INTO aspect (id, description) values (4, 'Ecology');
+INSERT INTO aspect (id, description) values (5, 'Treaty');
 
 
 
@@ -57,46 +60,109 @@ INSERT INTO referral_status (id, description) values (4, 'Denied');
 
 
 -- ----------------------------------------------------------------------------
+-- Table: REFERRAL TYPE
+-- ----------------------------------------------------------------------------
+CREATE TABLE referral_type(
+	id serial NOT NULL,
+	description character varying NOT NULL,
+	CONSTRAINT referral_type_pkey PRIMARY KEY (id)
+) WITH (OIDS=FALSE);
+
+ALTER TABLE referral_type OWNER TO ktunaxa;
+GRANT ALL ON TABLE referral_type TO ktunaxa;
+GRANT ALL ON TABLE referral_type TO postgres;
+
+INSERT INTO referral_type (id, description) values (1, 'Adventure Tourism');
+INSERT INTO referral_type (id, description) values (2, 'Agriculture');
+INSERT INTO referral_type (id, description) values (3, 'Aquaculture');
+INSERT INTO referral_type (id, description) values (4, 'Clean Energy');
+INSERT INTO referral_type (id, description) values (5, 'Commercial');
+INSERT INTO referral_type (id, description) values (6, 'Communication Sites');
+INSERT INTO referral_type (id, description) values (7, 'Community & Institutional');
+INSERT INTO referral_type (id, description) values (8, 'Contaminated Sites and Restoration');
+INSERT INTO referral_type (id, description) values (9, 'Land Sales');
+INSERT INTO referral_type (id, description) values (10, 'Flood Protection');
+INSERT INTO referral_type (id, description) values (11, 'Forestry');
+INSERT INTO referral_type (id, description) values (12, 'Grazing');
+INSERT INTO referral_type (id, description) values (13, 'Guide Outfitting');
+INSERT INTO referral_type (id, description) values (14, 'Industrial');
+INSERT INTO referral_type (id, description) values (15, 'Mining: Placer');
+INSERT INTO referral_type (id, description) values (16, 'Mining: Aggregate and Quarry');
+INSERT INTO referral_type (id, description) values (17, 'Mining: Mine Development');
+INSERT INTO referral_type (id, description) values (18, 'Oil and Gas');
+INSERT INTO referral_type (id, description) values (19, 'Private Moorage');
+INSERT INTO referral_type (id, description) values (20, 'Property Development');
+INSERT INTO referral_type (id, description) values (21, 'Public Recreation - Parks');
+INSERT INTO referral_type (id, description) values (22, 'Public Recreation - General');
+INSERT INTO referral_type (id, description) values (23, 'Residential');
+INSERT INTO referral_type (id, description) values (24, 'Resort Development');
+INSERT INTO referral_type (id, description) values (25, 'Roads');
+INSERT INTO referral_type (id, description) values (26, 'Utilities');
+
+
+
+-- ----------------------------------------------------------------------------
+-- Table: REFERRAL APPLICATION TYPE
+-- ----------------------------------------------------------------------------
+CREATE TABLE referral_application_type(
+	id serial NOT NULL,
+	description character varying NOT NULL,
+	CONSTRAINT referral_application_type_pkey PRIMARY KEY (id)
+) WITH (OIDS=FALSE);
+
+ALTER TABLE referral_application_type OWNER TO ktunaxa;
+GRANT ALL ON TABLE referral_application_type TO ktunaxa;
+GRANT ALL ON TABLE referral_application_type TO postgres;
+
+INSERT INTO referral_application_type (id, description) values (1, 'New');
+INSERT INTO referral_application_type (id, description) values (2, 'Renewal');
+INSERT INTO referral_application_type (id, description) values (3, 'Amendment');
+INSERT INTO referral_application_type (id, description) values (4, 'Replacement');
+
+
+
+-- ----------------------------------------------------------------------------
 -- Table: REFERRAL
 -- ----------------------------------------------------------------------------
 CREATE TABLE referral(
 	id serial NOT NULL,
-	title character varying NOT NULL,
-	description character varying NOT NULL,
-	creation_date timestamp NOT NULL,
-	created_by character varying,
-	status_id bigint NOT NULL,
+	land_referral_id character varying,
+	applicant_name character varying NOT NULL,
+	project_name character varying NOT NULL,
+	project_description character varying NOT NULL,
+	project_location character varying NOT NULL,
+	project_background character varying,
+	external_project_id character varying,
+	external_file_id character varying,
+	external_agency_name character varying NOT NULL,
 	contact_name character varying NOT NULL,
 	contact_email character varying NOT NULL,
+	contact_phone character varying NOT NULL,
+	contact_address character varying,
+	type_id bigint NOT NULL,
+	application_type_id bigint NOT NULL,
+	confidential boolean NOT NULL DEFAULT FALSE,
+	receive_date timestamp NOT NULL,
+	response_date timestamp NOT NULL,
+	active_retention_period int NOT NULL,
+	semi_active_retention_period int NOT NULL,
+	final_disposition int NOT NULL,
+	assessment_level int,
+	record_classification character varying NOT NULL,
+	status_id bigint NOT NULL,
 	geom geometry,
 	CONSTRAINT enforce_dims_geom CHECK ((ndims(geom) = 2)),
 --	CONSTRAINT enforce_geotype_geom CHECK (((geometrytype(geom) = 'POINT'::text) OR (geom IS NULL))),
 --	CONSTRAINT enforce_srid_geom CHECK ((srid(geom) = 900913)),
 	CONSTRAINT referral_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_referral_type FOREIGN KEY (type_id) REFERENCES referral_type (id),
+	CONSTRAINT fk_referral_application_type FOREIGN KEY (application_type_id) REFERENCES referral_application_type (id),
 	CONSTRAINT fk_referral_status FOREIGN KEY (status_id) REFERENCES referral_status (id)
 ) WITH (OIDS=FALSE);
 
 ALTER TABLE referral OWNER TO ktunaxa;
 GRANT ALL ON TABLE referral TO ktunaxa;
 GRANT ALL ON TABLE referral TO postgres;
-
-
-
--- ----------------------------------------------------------------------------
--- Table: DEADLINE
--- ----------------------------------------------------------------------------
-CREATE TABLE deadline(
-	id serial NOT NULL,
-	description character varying NOT NULL,
-	deadline_date timestamp NOT NULL,
-	referral_id bigint NOT NULL,
-	CONSTRAINT deadline_pkey PRIMARY KEY (id),
-	CONSTRAINT fk_deadline_referral FOREIGN KEY (referral_id) REFERENCES referral (id)
-) WITH (OIDS=FALSE);
-
-ALTER TABLE deadline OWNER TO ktunaxa;
-GRANT ALL ON TABLE deadline TO ktunaxa;
-GRANT ALL ON TABLE deadline TO postgres;
 
 
 
@@ -156,12 +222,37 @@ GRANT ALL ON TABLE reference_key_value TO postgres;
 
 
 -- ----------------------------------------------------------------------------
+-- Table: DOCUMENT TYPE
+-- ----------------------------------------------------------------------------
+CREATE TABLE document_type(
+	id serial NOT NULL,
+	description character varying NOT NULL,
+	CONSTRAINT document_type_pkey PRIMARY KEY (id)
+) WITH (OIDS=FALSE);
+
+ALTER TABLE document_type OWNER TO ktunaxa;
+GRANT ALL ON TABLE document_type TO ktunaxa;
+GRANT ALL ON TABLE document_type TO postgres;
+
+INSERT INTO document_type (id, description) values (1, 'KLRA Response');
+INSERT INTO document_type (id, description) values (2, 'KLRA General Communication');
+INSERT INTO document_type (id, description) values (3, 'KLRA Community Input');
+INSERT INTO document_type (id, description) values (4, 'KLRA Request for Information');
+INSERT INTO document_type (id, description) values (5, 'External - Initial Referral Notification');
+INSERT INTO document_type (id, description) values (6, 'External - General Communication');
+INSERT INTO document_type (id, description) values (7, 'External - Request for Information');
+
+
+
+-- ----------------------------------------------------------------------------
 -- Table: DOCUMENT
 -- ----------------------------------------------------------------------------
 CREATE TABLE document(
 	id serial NOT NULL,
 	title character varying NOT NULL,
 	description character varying NOT NULL,
+	keywords character varying,
+	type_id bigint NOT NULL,
 	document_id character varying NOT NULL,
 	addition_date timestamp NOT NULL,
 	added_by character varying,
@@ -169,6 +260,7 @@ CREATE TABLE document(
 	include_in_report boolean NOT NULL DEFAULT false,
 	referral_id bigint NOT NULL,
 	CONSTRAINT document_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_document_type FOREIGN KEY (type_id) REFERENCES document_type (id),
 	CONSTRAINT fk_document_referral FOREIGN KEY (referral_id) REFERENCES referral (id)
 ) WITH (OIDS=FALSE);
 
@@ -179,31 +271,10 @@ GRANT ALL ON TABLE document TO postgres;
 
 
 -- ----------------------------------------------------------------------------
--- Table: COMMENT
--- ----------------------------------------------------------------------------
--- CREATE TABLE comment(
--- 	id serial NOT NULL,
--- 	title character varying NOT NULL,
--- 	content character varying NOT NULL,
--- 	creation_date timestamp NOT NULL,
--- 	created_by character varying,
--- 	include_in_report boolean NOT NULL DEFAULT false,
--- 	checked_content character varying,
--- 	CONSTRAINT comment_pkey PRIMARY KEY (id)
--- ) WITH (OIDS=FALSE);
-
--- ALTER TABLE comment OWNER TO ktunaxa;
--- GRANT ALL ON TABLE comment TO ktunaxa;
--- GRANT ALL ON TABLE comment TO postgres;
-
-
-
--- ----------------------------------------------------------------------------
 -- Table: REFERRAL COMMENT
 -- ----------------------------------------------------------------------------
 CREATE TABLE referral_comment(
 	id serial NOT NULL,
---	comment_id bigint NOT NULL,
 	title character varying NOT NULL,
 	content character varying NOT NULL,
 	creation_date timestamp NOT NULL,
@@ -212,7 +283,6 @@ CREATE TABLE referral_comment(
 	checked_content character varying,
 	referral_id bigint NOT NULL,
 	CONSTRAINT referral_comment_pkey PRIMARY KEY (id),
---	CONSTRAINT fk_referral_comment_comment FOREIGN KEY (comment_id) REFERENCES comment (id),
 	CONSTRAINT fk_referral_comment_referral FOREIGN KEY (referral_id) REFERENCES referral (id)
 ) WITH (OIDS=FALSE);
 
@@ -227,7 +297,6 @@ GRANT ALL ON TABLE referral_comment TO postgres;
 -- ----------------------------------------------------------------------------
 CREATE TABLE document_comment(
 	id serial NOT NULL,
---	comment_id bigint NOT NULL,
 	title character varying NOT NULL,
 	content character varying NOT NULL,
 	creation_date timestamp NOT NULL,
@@ -236,7 +305,6 @@ CREATE TABLE document_comment(
 	checked_content character varying,
 	document_id bigint NOT NULL,
 	CONSTRAINT document_comment_pkey PRIMARY KEY (id),
---	CONSTRAINT fk_document_comment_comment FOREIGN KEY (comment_id) REFERENCES comment (id),
 	CONSTRAINT fk_document_comment_document FOREIGN KEY (document_id) REFERENCES document (id)
 ) WITH (OIDS=FALSE);
 
