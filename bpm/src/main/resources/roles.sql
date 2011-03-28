eaa86c35ffe178d273a3035c28a09ded084dc3da
joachimvda <joachimvda@cebd4ea5-dd72-4036-80b6-269358829854>
2011-03-28 20:23:36 +0000

    KTU-18 initial deployable version
    git-svn-id: https://svn.geomajas.org/geosparc/projects/ktunaxa@883 cebd4ea5-dd72-4036-80b6-269358829854

---
+++ bpm/src/main/resources/roles.sql
@@ -0,0 +1,13 @@
+insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'evaluateEvaluator', 1, 'evaluateEvaluator', 'assignment');
+insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'treatyEvaluator', 1, 'treatyEvaluator', 'assignment');
+insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'culturalEvaluator', 1, 'culturalEvaluator', 'assignment');
+insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'ecologyEvaluator', 1, 'ecologyEvaluator', 'assignment');
+insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'aquaticEvaluator', 1, 'aquaticEvaluator', 'assignment');
+insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'archaeologyEvaluator', 1, 'archaeologyEvaluator', 'assignment');
+
+insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'evaluateEvaluator' );
+insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'treatyEvaluator' );
+insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'culturalEvaluator' );
+insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'ecologyEvaluator' );
+insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'aquaticEvaluator' );
+insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'archaeologyEvaluator' );
