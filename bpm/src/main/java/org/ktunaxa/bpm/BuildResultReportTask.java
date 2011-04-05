5ee5a7b06861516a3b3c06126449ecad7246095c
joachimvda <joachimvda@cebd4ea5-dd72-4036-80b6-269358829854>
2011-04-05 13:49:43 +0000

    KTU-18 base working flows
    git-svn-id: https://svn.geomajas.org/geosparc/projects/ktunaxa@908 cebd4ea5-dd72-4036-80b6-269358829854

---
+++ bpm/src/main/java/org/ktunaxa/bpm/BuildResultReportTask.java
@@ -0,0 +1,28 @@
+/*
+ * This is part of the Ktunaxa referral system.
+ *
+ * Copyright 2011 Geosparc nv, http://www.geosparc.com/, Belgium.
+ * Copyright 2011 Ktunaxa Nation Counsil, http://www.ktunaxa.org/, Canada.
+ *
+ * The program is available in open source according to the GNU Affero
+ * General Public License. All contributions in this program are covered
+ * by the Geomajas Contributors License Agreement. For full licensing
+ * details, see LICENSE.txt in the project root.
+ */
+
+package org.ktunaxa.bpm;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+/**
+ * Activiti task which builds the result report.
+ *
+ * @author Joachim Van der Auwera
+ */
+public class BuildResultReportTask implements JavaDelegate {
+
+	public void execute(DelegateExecution delegateExecution) throws Exception {
+		// @todo implement
+	}
+}
--- bpm/src/main/resources/diagrams/roles.sql
+++ bpm/src/main/resources/diagrams/roles.sql
@@ -4,6 +4,8 @@ insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'culturalEvaluator',
 insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'ecologyEvaluator', 1, 'ecologyEvaluator', 'assignment');
 insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'aquaticEvaluator', 1, 'aquaticEvaluator', 'assignment');
 insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'archaeologyEvaluator', 1, 'archaeologyEvaluator', 'assignment');
+insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'communityManager', 1, 'communityManager', 'assignment');
+insert into ACT_ID_GROUP (ID_, REV_, NAME_, TYPE_) values ( 'referralManager', 1, 'referralManager', 'assignment');
 
 insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'evaluateEvaluator' );
 insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'treatyEvaluator' );
@@ -11,3 +13,5 @@ insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'cultural
 insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'ecologyEvaluator' );
 insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'aquaticEvaluator' );
 insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'archaeologyEvaluator' );
+insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'communityManager' );
+insert into ACT_ID_MEMBERSHIP (USER_ID_, GROUP_ID_) values ( 'kermit', 'referralManager' );
