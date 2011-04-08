29d8382c2405d38caaa084e921381efdfd71ed9e
joachimvda <joachimvda@cebd4ea5-dd72-4036-80b6-269358829854>
2011-04-08 00:35:39 +0000

    KTU-18 logging on service task (for now)
    git-svn-id: https://svn.geomajas.org/geosparc/projects/ktunaxa@922 cebd4ea5-dd72-4036-80b6-269358829854

--- bpm/src/main/java/org/ktunaxa/bpm/BuildResultReportTask.java
+++ bpm/src/main/java/org/ktunaxa/bpm/BuildResultReportTask.java
@@ -8,6 +8,8 @@ package org.ktunaxa.bpm;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Activiti task which builds the result report.
@@ -16,7 +18,10 @@ import org.activiti.engine.delegate.JavaDelegate;
  */
 public class BuildResultReportTask implements JavaDelegate {
 
+	private final Logger log  = LoggerFactory.getLogger(BuildResultReportTask.class);
+
 	public void execute(DelegateExecution delegateExecution) throws Exception {
+		log.debug("Start task execution " + delegateExecution.getVariable("referralId"));
 		// @todo implement
 	}
 }
