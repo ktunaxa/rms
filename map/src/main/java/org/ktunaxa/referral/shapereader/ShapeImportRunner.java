80668683e27765833aaa3698614d86ede6df26fe
jandm <jandm@cebd4ea5-dd72-4036-80b6-269358829854>
2011-06-28 09:23:38 +0000

    KTU-153: incomplete context + logging level to ERROR + unit test fix
    git-svn-id: https://svn.geomajas.org/geosparc/projects/ktunaxa@1243 cebd4ea5-dd72-4036-80b6-269358829854

--- map/src/main/java/org/ktunaxa/referral/shapereader/ShapeImportRunner.java
+++ map/src/main/java/org/ktunaxa/referral/shapereader/ShapeImportRunner.java
@@ -28,8 +28,10 @@ public final class ShapeImportRunner {
 	/** Initialize the object, by preparing a Spring context. */
 	private ShapeImportRunner() {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"classpath:org/geomajas/spring/geomajasContext.xml",
 				"classpath:org/ktunaxa/referral/shapereader/spring-hibernate.xml",
-				"classpath:org/ktunaxa/referral/shapereader/applicationContext.xml");
+				"classpath:org/ktunaxa/referral/shapereader/applicationContext.xml",
+				"classpath:org/ktunaxa/referral/shapereader/bpm.xml");
 		service = ctx.getBean(ShapeReaderService.class);
 	}
 
