d2f774025cfa49749548c6c9a41407b42c7889d0
joachimvda <joachimvda@cebd4ea5-dd72-4036-80b6-269358829854>
2011-06-30 18:39:01 +0000

    KTU-95 sort tasks
    git-svn-id: https://svn.geomajas.org/geosparc/projects/ktunaxa@1261 cebd4ea5-dd72-4036-80b6-269358829854

--- map/src/main/java/org/ktunaxa/referral/client/gui/TaskListView.java
+++ map/src/main/java/org/ktunaxa/referral/client/gui/TaskListView.java
@@ -6,11 +6,13 @@
 
 package org.ktunaxa.referral.client.gui;
 
+import org.ktunaxa.bpm.KtunaxaBpmConstant;
 import org.ktunaxa.referral.client.widget.AbstractCollapsibleListBlock;
 import org.ktunaxa.referral.client.widget.ListView;
 import org.ktunaxa.referral.server.dto.TaskDto;
 
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,10 +27,57 @@ public class TaskListView extends ListView<TaskDto> {
 
 	static {
 		SORT_ATTRIBUTES = new HashMap<String, Comparator<AbstractCollapsibleListBlock<TaskDto>>>();
-		//SORT_ATTRIBUTES.put("date", new DateComparator());
+		SORT_ATTRIBUTES.put("deadline", new DeadlineComparator());
+		SORT_ATTRIBUTES.put("referral", new ReferralComparator());
+		SORT_ATTRIBUTES.put("creation", new CreationComparator());
 	}
 
 	public TaskListView() {
 		super(false, true, SORT_ATTRIBUTES);
+	}
+
+	/**
+	 * Comparator for sorting on the completion deadline.
+	 *
+	 * @author Joachim Van der Auwera
+	 */
+	private static class DeadlineComparator implements Comparator<AbstractCollapsibleListBlock<TaskDto>> {
+
+		public int compare(AbstractCollapsibleListBlock<TaskDto> left,
+				AbstractCollapsibleListBlock<TaskDto> right) {
+			String leftValue = left.getObject().getVariables().get(KtunaxaBpmConstant.VAR_COMPLETION_DEADLINE);
+			String rightValue = right.getObject().getVariables().get(KtunaxaBpmConstant.VAR_COMPLETION_DEADLINE);
+			return leftValue.compareTo(rightValue);
+		}
+	}
+
+	/**
+	 * Comparator for sorting on the referral id.
+	 *
+	 * @author Joachim Van der Auwera
+	 */
+	private static class ReferralComparator implements Comparator<AbstractCollapsibleListBlock<TaskDto>> {
+
+		public int compare(AbstractCollapsibleListBlock<TaskDto> left,
+				AbstractCollapsibleListBlock<TaskDto> right) {
+			String leftValue = left.getObject().getVariables().get(KtunaxaBpmConstant.VAR_REFERRAL_ID);
+			String rightValue = right.getObject().getVariables().get(KtunaxaBpmConstant.VAR_REFERRAL_ID);
+			return leftValue.compareTo(rightValue);
+		}
+	}
+
+	/**
+	 * Comparator for sorting on the creation time.
+	 *
+	 * @author Joachim Van der Auwera
+	 */
+	private static class CreationComparator implements Comparator<AbstractCollapsibleListBlock<TaskDto>> {
+
+		public int compare(AbstractCollapsibleListBlock<TaskDto> left,
+				AbstractCollapsibleListBlock<TaskDto> right) {
+			Date leftValue = left.getObject().getCreateTime();
+			Date rightValue = right.getObject().getCreateTime();
+			return leftValue.compareTo(rightValue);
+		}
 	}
 }
