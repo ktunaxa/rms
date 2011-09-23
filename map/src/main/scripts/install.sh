faa1d014a9c91edc1f6028eaddd6f03c426bca5f
joachimvda <joachimvda@cebd4ea5-dd72-4036-80b6-269358829854>
2011-09-23 06:05:18 +0000

    update install script to copy on Ktunaxa server, takes backup at beginning and does db update
    git-svn-id: https://svn.geomajas.org/geosparc/projects/ktunaxa@1455 cebd4ea5-dd72-4036-80b6-269358829854

--- map/src/main/scripts/install.sh
+++ map/src/main/scripts/install.sh
@@ -1,6 +1,8 @@
+export BAKTS="`date +%Y%m%d-%H%M`"
+cp /var/lib/tomcat-6/webapps/map.war ./map.$BAKTS.bak.war
 cd ~geosparc/ktunaxa
 svn update
-mvn -DskipTests clean install
+mvn -U -DskipTests -Dcheckstyle.skip clean install
 cd ~geosparc/ktunaxa/deploy
 mvn -P ktunaxa clean process-resources
 sudo /etc/init.d/tomcat6 stop
@@ -10,6 +12,7 @@ sudo rm -rf /var/lib/tomcat-6/webapps/map
 sudo rm -rf /var/lib/tomcat-6/webapps/alfresco
 sudo rm -rf /var/lib/tomcat-6/temp /var/lib/tomcat-6/work
 sudo cp /home/geosparc/ktunaxa/deploy/target/*.war /var/lib/tomcat-6/webapps
+cd ../map
+mvn -Pupdatedb process-resources
 sudo /etc/init.d/tomcat6 start
-
 
