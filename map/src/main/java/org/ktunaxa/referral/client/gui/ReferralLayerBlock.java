47fda508729f81a945fc2f1e9f95b84e8597ab18
joachimvda <joachimvda@cebd4ea5-dd72-4036-80b6-269358829854>
2011-12-20 09:50:57 +0000

    KTU-178 filter referrals, on status
    git-svn-id: https://svn.geomajas.org/geosparc/projects/ktunaxa@1886 cebd4ea5-dd72-4036-80b6-269358829854

--- map/src/main/java/org/ktunaxa/referral/client/gui/ReferralLayerBlock.java
+++ map/src/main/java/org/ktunaxa/referral/client/gui/ReferralLayerBlock.java
@@ -25,6 +25,8 @@ public class ReferralLayerBlock extends LayerBlock {
 	private VectorLayer layer;
 	private DynamicForm form = new DynamicForm();
 	ComboBoxItem status = new ComboBoxItem("Status");
+	ComboBoxItem agency = new ComboBoxItem("ExternalAgencyType");
+	ComboBoxItem type = new ComboBoxItem("Type");
 
 	public ReferralLayerBlock(Layer<?> referral) {
 		super(referral);
@@ -33,16 +35,77 @@ public class ReferralLayerBlock extends LayerBlock {
 		ChangedHandler filterChangedHandler = new FilterChangedHandler();
 
 		form.setWidth100();
+		// @todo hardcoded options, ideally the options should be read from the database when the application starts
 		LinkedHashMap<String, String> statusValues = new LinkedHashMap<String, String>();
 		statusValues.put("", "any");
 		statusValues.put("1", "Incompletely created");
 		statusValues.put("2", "In progress");
 		statusValues.put("3", "Finished");
 		statusValues.put("4", "Stopped");
+		status.setTitle("Status");
 		status.setValueMap(statusValues);
 		status.setValue("");
 		status.addChangedHandler(filterChangedHandler);
-		form.setFields(status);
+		// @todo hardcoded options, ideally the options should be read from the database when the application starts
+		LinkedHashMap<String, String> agencyValues = new LinkedHashMap<String, String>();
+		agencyValues.put("", "any");
+		agencyValues.put("1", "BC Government");
+		agencyValues.put("2", "Government of Canada");
+		agencyValues.put("3", "Local Government");
+		agencyValues.put("4", "Industry");
+		agencyValues.put("5", "Other");
+		agency.setTitle("External Agency Type");
+		agency.setValueMap(agencyValues);
+		agency.setValue("");
+		agency.addChangedHandler(filterChangedHandler);
+		// @todo hardcoded options, ideally the options should be read from the database when the application starts
+		LinkedHashMap<String, String> typeValues = new LinkedHashMap<String, String>();
+		typeValues.put("", "any");
+		typeValues.put("1", "Adventure Tourism");
+		typeValues.put("2", "Agriculture");
+		typeValues.put("3", "Aquaculture");
+		typeValues.put("4", "Clean Energy");
+		typeValues.put("5", "Commercial");
+		typeValues.put("6", "Communication Sites");
+		typeValues.put("7", "Community & Institutional");
+		typeValues.put("8", "Contaminated Sites and Restoration");
+		typeValues.put("9", "Land Sales");
+		typeValues.put("10", "Flood Protection");
+		typeValues.put("11", "Forestry");
+		typeValues.put("12", "Grazing");
+		typeValues.put("13", "Guide Outfitting");
+		typeValues.put("14", "Industrial");
+		typeValues.put("15", "Mining: Placer");
+		typeValues.put("16", "Mining: Aggregate and Quarry");
+		typeValues.put("17", "Mining: Mine Development");
+		typeValues.put("19", "Private Moorage");
+		typeValues.put("20", "Property Development");
+		typeValues.put("21", "Public Recreation - Parks");
+		typeValues.put("22", "Public Recreation - General");
+		typeValues.put("23", "Residential");
+		typeValues.put("24", "Resort Development");
+		typeValues.put("25", "Roads/Bridges");
+		typeValues.put("26", "Utilities");
+		typeValues.put("27", "Crown Grant");
+		typeValues.put("28", "Mineral Exploration");
+		typeValues.put("29", "Oil and Gas Production");
+		typeValues.put("30", "Oil and Gas Exploration");
+		typeValues.put("31", "Oil and Gas Infrastructure");
+		typeValues.put("32", "Zoning, DPA or OCP Changes");
+		typeValues.put("33", "Subdivision Application");
+		typeValues.put("34", "Water Diversion");
+		typeValues.put("35", "Changes in/about stream/waterbody");
+		typeValues.put("36", "Waste Discharge");
+		typeValues.put("37", "Pest Management");
+		typeValues.put("38", "Assignment (name change)");
+		typeValues.put("39", "Follow up letter");
+		typeValues.put("40", "Multiple");
+		typeValues.put("41", "Other");
+		type.setTitle("Type");
+		type.setValueMap(typeValues);
+		type.setValue("");
+		type.addChangedHandler(filterChangedHandler);
+		form.setFields(status, agency, type);
 		addMember(form);
 	}
 
@@ -62,7 +125,25 @@ public class ReferralLayerBlock extends LayerBlock {
 				filter.append("status.id = ");
 				filter.append(statusValue);
 			}
-			
+
+			String agencyValue = agency.getValueAsString();
+			if (!"".equals(agencyValue)) {
+				if (filter.length() > 0) {
+					filter.append(" AND ");
+				}
+				filter.append("externalAgencyType.id = ");
+				filter.append(agencyValue);
+			}
+
+			String typeValue = type.getValueAsString();
+			if (!"".equals(typeValue)) {
+				if (filter.length() > 0) {
+					filter.append(" AND ");
+				}
+				filter.append("type.id = ");
+				filter.append(typeValue);
+			}
+
 			layer.setFilter(filter.toString());
 		}
 	}
