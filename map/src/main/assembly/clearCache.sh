53b2741f3802d34d4a6e29548ec64628aa11a391
Joachim Van der Auwera <joachim@geosparc.com>
2012-03-06 00:48:19 +0100

    fix cache location

--- map/src/main/assembly/clearCache.sh
+++ map/src/main/assembly/clearCache.sh
@@ -1,3 +1,5 @@
 sudo /etc/init.d/tomcat6 stop
-sudo rm -rf /tmp/tomcat-tmp/geomajas
+sudo rm -rf /opt/tomcat-6/temp
+sudo rm -rf /opt/tomcat-6/work
+sudo rm -rf /tmp/geomajas
 sudo /etc/init.d/tomcat6 start
